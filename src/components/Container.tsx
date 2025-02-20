7af21f2d61ff4ba95263b1806ef4402a275856f2
Oluwadara <maxegunjobi@gmail.com>
2025-02-20 12:28:52 +0100

    ok

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -35,7 +35,7 @@ const Container = () => {
       <Header />
 
       {/* Chat Output Wrapper */}
-      <div className="h-0 flex-grow">
+      <div className="h-0 flex-grow overflow-y-auto">
         {/* ✅ Show ChatOutput if `hasMessages` is true, otherwise show DefaultChatOutput */}
         {/* {hasMessages ? <ChatOutput /> : <DefaultChatOutput />} */}
         <DefaultChatOutput />
